bee0e0d80124230edc4579c77041343d80ee6693
Anton <gulley_rapier0v@icloud.com>
2022-06-11 23:33:53 +0300

    done

--- for GeekBrains/lesson_1_hw_2.py
+++ for GeekBrains/lesson_1_hw_2.py
@@ -1,7 +1,7 @@
 # 2. Пользователь вводит время в секундах. Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
 
 time_in_sec = int(input ('Введите время в секундах: '))
-hour = int(time_in_sec /3600)
-minutes = (time_in_sec - hour * 3600) // 60
+hours = int(time_in_sec /3600)
+minutes = (time_in_sec - hours * 3600) // 60
 seconds = time_in_sec %60
-print ("%.2d" % hour,":", "%.2d" % minutes,":", "%.2d" % seconds)
+print ("%.2d" % hours,":", "%.2d" % minutes,":", "%.2d" % seconds)
