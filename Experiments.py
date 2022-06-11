21ebd350e936260de7854a37a1a8cd10c59f91fc
Anton <gulley_rapier0v@icloud.com>
2022-06-11 12:48:48 +0300

    null

---
+++ Experiments.py
@@ -0,0 +1,32 @@
+#print ('Привет')
+
+#name ='Вася'
+#age = 2
+#period=7
+#print('Попугайлчик')
+#print(name)
+#print('в')
+#print(age)
+#print('научился говорить')
+#print('свое имя')
+#print(name)
+#print('через')
+#print(period)
+#print('года')
+#print(age+period)
+#person_name = 'Max'
+#print(type(person_name))
+
+#age = 30
+#print(type(age))
+
+#period = 3.2
+#print(type(period))
+
+#name=input('Как тебя зовут: ')
+#print('Привет', name)
+
+#age=int(input('Сколько Вам лет: '))
+#period=5
+#age_period=age+period
+#rint('через', period, 'лет Вам будет', age_period)
