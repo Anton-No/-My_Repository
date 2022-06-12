d47d70551e0f36f32c95e7664a51d88cea7fbdcc
Anton <gulley_rapier0v@icloud.com>
2022-06-12 17:38:13 +0300

    done

--- for GeekBrains/lesson_1_hw_4.py
+++ for GeekBrains/lesson_1_hw_4.py
@@ -1,20 +1,10 @@
 #4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе. Для решения используйте цикл while и арифметические операции.
 
 number = int( input( "Введите целое положительное число, "))
-
 maximum = 0
 while number > 0:
      last = number %10
      if last > maximum:
           maximum = last
      number = number //10
-
 print( "Максимальное число:", maximum)
-
-
-
-
-
-#Вариант 2 (простой)
-#max_number = max( tuple(number))
-#print  (max_number)
