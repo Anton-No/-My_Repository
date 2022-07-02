2efe896f08336ffee97280dfa6c78028fea8626f
Anton <gulley_rapier0v@icloud.com>
2022-07-02 19:14:54 +0300

    done

--- for GeekBrains/lesson_5/lesson_5_hw_4.py
+++ for GeekBrains/lesson_5/lesson_5_hw_4.py
@@ -7,3 +7,11 @@
 
 #Напишите программу, открывающую файл на чтение и считывающую построчно данные. При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
 
+
+d = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+with open('/Users/an/Documents/My_Repository/for GeekBrains/lesson_5/text_4.txt', 'r', encoding='utf-8') as f:
+    with open('text_4.txt', 'w', encoding='utf-8') as f2:
+        for line in f:
+            en, *num = line.split()
+            ru = d[en]
+            f2.write(' '.join([ru] + num) + '\n')
