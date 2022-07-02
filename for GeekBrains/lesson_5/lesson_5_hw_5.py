b08e98b9b1ef25859880421194b58c52469b949b
Anton <gulley_rapier0v@icloud.com>
2022-07-02 19:26:47 +0300

    Done

--- for GeekBrains/lesson_5/lesson_5_hw_5.py
+++ for GeekBrains/lesson_5/lesson_5_hw_5.py
@@ -2,3 +2,9 @@
 
 
 
+with open('text_5.txt', 'w', encoding='utf-8') as f:
+    f.write(f"{' '.join([str(i) for i in range(1, 25)])}")
+    
+with open('text_5.txt', 'r', encoding='utf-8') as f:
+    numbers = [int(i) for i in f.read().split()]
+    print(sum(numbers))
