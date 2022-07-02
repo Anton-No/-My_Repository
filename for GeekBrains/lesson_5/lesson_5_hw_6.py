216825cc0b8a19d899e7ecc1dd2a3eaf41b10448
Anton <gulley_rapier0v@icloud.com>
2022-07-02 19:36:37 +0300

    Done

--- for GeekBrains/lesson_5/lesson_5_hw_6.py
+++ for GeekBrains/lesson_5/lesson_5_hw_6.py
@@ -6,3 +6,18 @@
 #Физкультура: — 30(пр) —
 #Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
+
+
+d = dict()
+with open('/Users/an/Documents/My_Repository/for GeekBrains/lesson_5/text_6.txt', encoding='utf-8') as f:
+    for line in f:
+        name, rest = line.split(':')
+        rest = rest.split()
+        num = 0
+        for part in rest:
+            if '-' in part:
+                continue
+            nums, type = part.split( '(')
+            num += int(nums)
+        d[name] = num
+print(d)
