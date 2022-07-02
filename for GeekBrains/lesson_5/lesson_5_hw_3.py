87185cf450099d109e50cccf73b12ef1122c4d81
Anton <gulley_rapier0v@icloud.com>
2022-07-02 18:50:42 +0300

    done

--- for GeekBrains/lesson_5/lesson_5_hw_3.py
+++ for GeekBrains/lesson_5/lesson_5_hw_3.py
@@ -4,3 +4,20 @@
 
 #Иванов 23543.12
 #Петров 13749.32
+
+
+
+with open('/Users/an/Documents/My_Repository/for GeekBrains/lesson_5/text_3.txt', 'r', encoding='utf-8') as f:
+    names = []
+    av_income = 0
+    num = 0
+    for line in f:
+        num += 1
+        name, income = (i for i in line.split())
+        income = float(income)
+        if income < 20000:
+            names.append(name)
+        av_income += income
+    av_income /= num
+print(*names)
+print(av_income)
