b5de01c9bebea76884dd4f0626e07aeb48bf1298
Anton <gulley_rapier0v@icloud.com>
2022-07-03 17:33:53 +0300

    Done

--- for GeekBrains/lesson_6/lesson_6_hw_2.py
+++ for GeekBrains/lesson_6/lesson_6_hw_2.py
@@ -8,3 +8,15 @@
 #    проверить работу метода.
 
 #Например: 20 м*5000 м*25 кг*5 см = 12500 т.
+
+
+
+class Road:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+    def mass_calculation(self, weight=25, thickness=5):
+        return f'Massa_asfalta: {(self._length * self._width * weight * thickness) / 1000} т.'
+
+road_1 = Road(5000, 20)
+print(road_1.mass_calculation())
